4c7d20fb00158e25882da76dfadc3e8c2f50b263
Sara Boyer <sara.jane.boyer@gmail.com>
2020-04-09 18:25:40 -0600

    userprofile and navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink, } from 'react-router-dom';
+import { Menu, } from 'semantic-ui-react';
+
+const Navbar = () => (
+  <Menu>
+    <NavLink to = '/'>
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </NavLink>
+    <NavLink to = '/user/profile'>
+      <Menu.Item>
+        username
+      </Menu.Item>
+    </NavLink>
+  </Menu>
+)
